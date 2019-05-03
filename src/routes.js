2fed5c194455255d2f8c43c7b27eac91a766e043
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-02 22:05:23 -0300

    Upload e Login
    Multer, upload de imagem e validação de login

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
+
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 const routes = express.Router()
 
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.post)
+
 routes.get('/signup', UserController.create)
-routes.post('/signup', UserController.post)
+routes.post('/signup', upload.single('avatar'), UserController.post)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = routes
