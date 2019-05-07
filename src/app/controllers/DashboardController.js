5180728a0b6edf3ee3a78a6a40b7bc1297ce24cd
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-06 21:03:55 -0300

    Dashboard list filter
    Deixa de exibir o próprio user na lista de providers caso o mesmo seja um

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,8 +1,16 @@
 const { User } = require('../models')
+const { Op } = require('sequelize')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    const providers = await User.findAll({
+      where: {
+        provider: true,
+        email: {
+          [Op.ne]: req.session.user.email
+        }
+      }
+    })
 
     return res.render('dashboard', { providers })
   }
